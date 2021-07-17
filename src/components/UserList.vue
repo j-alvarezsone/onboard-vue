dcb9d155f53bd12892b1bb86ff56f2c3f877f806
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-17 17:16:10 +0200

    added loading spinner

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mx-6 lg:mx-4">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mx-6 lg:mx-4" v-if="users.length">
     <user-card v-for="user in users" :key="user.id" :users="user" />
+  </div>
+  <div class="flex justify-center items-center h-screen" v-else>
+    <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
   </div>
 </template>
 <script>
