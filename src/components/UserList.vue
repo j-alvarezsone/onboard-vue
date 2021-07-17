ea137362746b97668b165b5d6d42629f5b2e4cc1
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-17 16:40:42 +0200

    get users from state and passed via props to user-card component

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-5 mx-6 lg:mx-4">
-    <user-card />
+    <user-card v-for="user in users" :key="user.id" :users="user" />
   </div>
 </template>
 <script>
@@ -10,6 +10,11 @@ export default {
   name: 'UserList',
   components: {
     UserCard,
+  },
+  computed: {
+    users() {
+      return this.$store.state.users;
+    },
   },
   mounted() {
     this.$store.dispatch('getUsers');
