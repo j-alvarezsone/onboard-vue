cc70d7617072185df7e9d2bd546d895129ee2080
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-17 15:40:42 +0200

    added store globally

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import DataddoTask from './DataddoTask.vue';
 import axios from 'axios';
+import store from './store';
 import './assets/css/tailwind.css';
 
 const app = createApp(DataddoTask);
 app.provide('$axios', axios);
+app.use(store);
 
 app.mount('#app');
