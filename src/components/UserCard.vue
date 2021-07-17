b559b7856686db123ab5eb928190aac666a52e9c
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-17 22:16:16 +0200

    added comment for user company name

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -19,6 +19,7 @@
           </div>
           <br />
           <br />
+          <!-- User company -->
           <h3 class="text-center text-xl text-gray-900 font-medium leading-8">{{ users.company.name }}</h3>
 
           <br />
