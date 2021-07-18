c17f60ead14a6c0cbf1e808247157044c0c9019a
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-18 15:13:29 +0200

    created SearchBox

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex my-6">
+    <input
+      class="rounded p-2 w-3/5 border-2 border-gray-200"
+      type="text"
+      placeholder="Search user by name..."
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: 'SearchBox',
+  props: {
+    modelValue: {
+      type: String,
+    },
+  },
+};
+</script>
