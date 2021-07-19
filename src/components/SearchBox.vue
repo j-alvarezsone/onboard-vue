9d5cd8577c7c66e706e5f33e384ae78bca1ab91b
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-19 14:22:20 +0200

    added selectedUsers and unselectedUsers props to get info from parent. changed css

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,20 +1,40 @@
 <template>
-  <div class="flex my-6">
+  <div class="w-full">
+    <!-- input -->
     <input
-      class="rounded p-2 w-3/5 border-2 border-gray-200"
+      class="rounded p-2 border-2 border-gray-200 w-full"
       type="text"
       placeholder="Search user by name..."
       :value="modelValue"
       @input="$emit('update:modelValue', $event.target.value)"
     />
   </div>
+  <!-- Selected -->
+  <div class="flex w-full ml-4">
+    <span class="w-full bg-blue-700 p-2 mr-4 rounded-md text-center text-white"
+      >Selected Users: {{ selectedUsers }}</span
+    >
+    <span class="w-full bg-blue-700 p-2 mr-4 rounded-md text-center text-white"
+      >Unselected Users: {{ unselectedUsers }}</span
+    >
+  </div>
 </template>
+
 <script>
 export default {
   name: 'SearchBox',
   props: {
     modelValue: {
       type: String,
+      required: true,
+    },
+    selectedUsers: {
+      type: Number,
+      required: true,
+    },
+    unselectedUsers: {
+      type: Number,
+      required: true,
     },
   },
 };
