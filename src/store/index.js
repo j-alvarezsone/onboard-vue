e544858a4d5dd63ee8203be19cc0746a6c3c4348
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-07-19 14:19:40 +0200

    added selected property to control selected user

--- src/store/index.js
+++ src/store/index.js
@@ -14,8 +14,12 @@ export default createStore({
     async getUsers({ commit }) {
       try {
         const resp = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const update = await resp.data.map((user) => ({
+          ...user,
+          selected: false,
+        }));
         setTimeout(() => {
-          commit('setUsers', resp.data);
+          commit('setUsers', update);
         }, 2000);
       } catch (error) {
         console.log(error);
